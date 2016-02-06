c98c900f19d7fb90b2be41d4b3be718ec3b282fa
hw <hussainweb@gmail.com>
2016-02-06 23:40:06 +0530

    Add tests for EntityCollection classes

--- tests/Entity/Collection/EntityCollectionTest.php
+++ tests/Entity/Collection/EntityCollectionTest.php
@@ -6,8 +6,17 @@
 
 namespace Hussainweb\DrupalApi\Tests\Entity\Collection;
 
+use Hussainweb\DrupalApi\Entity\Collection\CommentCollection;
 use Hussainweb\DrupalApi\Entity\Collection\EntityCollection;
+use Hussainweb\DrupalApi\Entity\Collection\NodeCollection;
+use Hussainweb\DrupalApi\Entity\Collection\PiftCiJobCollection;
+use Hussainweb\DrupalApi\Entity\Collection\TaxonomyTermCollection;
+use Hussainweb\DrupalApi\Entity\Collection\UserCollection;
 use Hussainweb\DrupalApi\Entity\Comment;
+use Hussainweb\DrupalApi\Entity\Node;
+use Hussainweb\DrupalApi\Entity\PiftCiJob;
+use Hussainweb\DrupalApi\Entity\TaxonomyTerm;
+use Hussainweb\DrupalApi\Entity\User;
 
 class EntityCollectionTest extends \PHPUnit_Framework_TestCase
 {
@@ -31,5 +40,35 @@ class EntityCollectionTest extends \PHPUnit_Framework_TestCase
         foreach ($collection as $id => $item) {
             $this->assertNotEmpty($item->getData()->cid);
         }
+    }
+
+    public function testCommentCollection()
+    {
+        $col = new CommentCollection((object) []);
+        $this->assertEquals(Comment::class, $col->getListItemClass());
+    }
+
+    public function testNodeCollection()
+    {
+        $col = new NodeCollection((object) []);
+        $this->assertEquals(Node::class, $col->getListItemClass());
+    }
+
+    public function testPiftCiJobCollection()
+    {
+        $col = new PiftCiJobCollection((object) []);
+        $this->assertEquals(PiftCiJob::class, $col->getListItemClass());
+    }
+
+    public function testTaxonomyTermCollection()
+    {
+        $col = new TaxonomyTermCollection((object) []);
+        $this->assertEquals(TaxonomyTerm::class, $col->getListItemClass());
+    }
+
+    public function testUserCollection()
+    {
+        $col = new UserCollection((object) []);
+        $this->assertEquals(User::class, $col->getListItemClass());
     }
 }
